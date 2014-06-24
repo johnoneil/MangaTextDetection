ca207555e0abaa4438eb19de5512ccd27b014d5b
on-three <on_three@outlook.com>
2014-06-23 22:19:52 -0700

    Started basic isolation of word balloons. needs work.

--- find_word_balloons.py
+++ find_word_balloons.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # vim: set ts=2 expandtab:
 """
-Module: filter_by_size_ar_test.py
+Module: find_word_balloons.py
 Desc:
 Author: John O'Neil
 Email: oneil.john@gmail.com
-DATE: Thursday, June 19th 2014
-
-  Step by step test of removing connected components first by size,
-  and then by aspect ratio.
+DATE: Sunday, June 22nd 2014
   
 """
 
@@ -37,37 +34,11 @@ def height_bb(a):
 def area_nz(slice, image):
   return np.count_nonzero(image[slice])
 
-def get_connected_components(image):
-  s = scipy.ndimage.morphology.generate_binary_structure(2,2)
-  labels,n = scipy.ndimage.measurements.label(image)#,structure=s)
-  objects = scipy.ndimage.measurements.find_objects(labels)
-  return objects
-
 def generate_connected_components(image):
   s = scipy.ndimage.morphology.generate_binary_structure(2,2)
   labels, num_labels = scipy.ndimage.measurements.label(image)#,structure=s)
   slices = scipy.ndimage.measurements.find_objects(labels)
   return (labels, num_labels, slices) 
-
-def bounding_boxes(image,connected_components,max_size,min_size):
-  mask = np.zeros(image.shape,'B')#np.uint8)#'B')
-  for component in connected_components:
-    if area_bb(component)**.5<min_size: continue
-    if area_bb(component)**.5>max_size: continue
-    #a = area_nz(component,image)
-    #if a<min_size: continue
-    #if a>max_size: continue
-    mask[component] = 1#255
-  return mask
-
-def masks(image,connected_components,max_size,min_size):
-  mask = zeros(image.shape,np.uint8)#,'B')
-  for component in connected_components:
-    if area_bb(component)**.5<min_size: continue
-    if area_bb(component)**.5>max_size: continue
-    mask[component] = image[component]>0
-  return mask
-
 
 '''
 def draw_bounding_boxes(img,connected_components,max_size=0,min_size=0,color=(0,0,255),line_size=2):
@@ -80,86 +51,14 @@ def draw_bounding_boxes(img,connected_components,max_size=0,min_size=0,color=(0,
     (ys,xs)=component[:2]
     cv2.rectangle(img,(xs.start,ys.start),(xs.stop,ys.stop),color,line_size)
 '''
-def filter_by_size(image,connected_components,max_size,min_size):
-  filtered = []
-  for cc in connected_components:
-    if area_bb(cc)<min_size: continue
-    if area_bb(cc)>max_size: continue
-    #if area_bb(cc)**0.5<min_size: continue
-    #if area_bb(cc)**0.5>max_size: continue
-    filtered.append(cc)
-  return filtered
-
-def filter_by_black_white_ratio(img,connected_components,maximum=1.0,minimum=0.0):
-  filtered = []
-  for component in connected_components:
-    black = area_nz(component,img)
-    a = area_bb(component) 
-    percent_black = float(black)/float(a)
-    if percent_black < minimum or percent_black > maximum:
-      #print 'component removed for percent ' + str(percent_black)
-      continue
-    filtered.append(component)
-  return filtered
-
-def average_size(img, minimum_area=3, maximum_area=100):
-  components = get_connected_components(img)
-  sorted_components = sorted(components,key=area_bb)
-  #sorted_components = sorted(components,key=lambda x:area_nz(x,binary))
-  areas = zeros(img.shape)
-  for component in sorted_components:
-    #As the input components are sorted, we don't overwrite
-    #a given area again (it will already have our max value)
-    if amax(areas[component])>0: continue
-    #take the sqrt of the area of the bounding box
-    areas[component] = area_bb(component)**0.5
-    #alternate implementation where we just use area of black pixels in cc
-    #areas[component]=area_nz(component,binary)
-  #we lastly take the median (middle value of sorted array) within the region of interest
-  #region of interest is defaulted to those ccs between 3 and 100 pixels on a side (text sized)
-  aoi = areas[(areas>minimum_area)&(areas<maximum_area)]
-  if len(aoi)==0:
-    return 0
-  return np.median(aoi)
-
-def mean_width(img, minimum=3, maximum=100):
-  components = get_connected_components(img)
-  sorted_components = sorted(components,key=area_bb)
-  widths = zeros(img.shape)
-  for c in sorted_components:
-    if amax(widths[c])>0: continue
-    widths[c]=width_bb(c)
-  aoi = widths[(widths>minimum)&(widths<maximum)]
-  if len(aoi)>0:
-    return np.mean(aoi)
-  return 0 
-
-def mean_height(img, minimum=3, maximum=100):
-  components = get_connected_components(img)
-  sorted_components = sorted(components,key=area_bb)
-  heights = zeros(img.shape)
-  for c in sorted_components:
-    if amax(heights[c])>0: continue
-    heights[c]=height_bb(c)
-  aoi = heights[(heights>minimum)&(heights<maximum)]
-  if len(aoi)>0:
-    return np.mean(aoi)
-  return 0 
-
-def form_mask(img, max_size, min_size):
-  components = get_connected_components(img)
-  sorted_components = sorted(components,key=area_bb)
-  #mask = bounding_boxes(img,sorted_components,max_size,min_size)
-  mask = masks(img,sorted_components,max_size,min_size)
-  return mask
 
 class AreaFilter(object):
   def __init__(self, min=10.0, max=100.0):
     self._min = min
     self._max = max
   def filter(self, component):
-    if area_bb(component)**.5<self._min: return False
-    if area_bb(component)**.5>self._max: return False
+    if self._min and area_bb(component)**.5<self._min: return False
+    if self._max and area_bb(component)**.5>self._max: return False
     return True
 
   def __call__(self, cc):
@@ -180,14 +79,24 @@ class AspectRatioFilter(object):
   def __call__(self, cc):
     return self.filter(cc)
 
-def generate_mask(image, filter):
+def generate_mask(image, filter, include_contained=True):
   (labels, num_labels, components) = generate_connected_components(image)
   mask = zeros(image.shape,np.uint8)#,'B')
   for label in range(num_labels):
     two_d_slice = components[label]
     if not filter(two_d_slice):
       continue
-    mask[two_d_slice] = labels[two_d_slice]==(label+1)
+    mask[two_d_slice] |= labels[two_d_slice]==(label+1)
+    #also add nonzero pixels from all connected components ENTIRELY CONTAINED
+    #by this cc's bounding box. This is an attempt to partially recover smaller
+    #character components which might not be connected with the primary character
+    #(i.e. marks and accent like forms)
+    if include_contained:
+      for l in range(num_labels):
+        if l == label: continue
+        other_slice = components[l]
+        if contains(two_d_slice, other_slice):
+          mask[other_slice] |= labels[other_slice]==(l+1)
   return mask
 
 def binarize(image, threshold=180):
@@ -198,6 +107,52 @@ def binarize(image, threshold=180):
   binary[high_values] = 255
   return binary
 
+
+def contains(cc_a, cc_b):
+  w = width_bb(cc_a)
+  dw = w/5
+  h = height_bb(cc_a)
+  dh = h/5
+  return cc_b[0].start>=(cc_a[0].start-dh) and cc_b[0].stop<=(cc_a[0].stop+dh) and \
+    cc_b[1].start>=(cc_a[1].start-dw) and cc_b[1].stop<=(cc_a[1].stop+dw)
+
+def find_balloons(cleaned_binary, white_areas):
+  balloons = []
+  #sort white areas from smallest to largest. We want smallest "hits"
+  (labels, num_labels, components) = generate_connected_components(white_areas)
+  (text_labels, text_num_labels, text_components) = generate_connected_components(cleaned_binary)
+  sorted_white_areas = sorted(components, key=area_bb)
+  for t in text_components:
+    #print str(area_bb(i))
+    #any white areas that contain a text hit, add them to our ballon candidates
+    #print 'num labels ' + str(num_labels)
+    for label in reversed(range(num_labels)):
+      print label
+      if contains(components[label], t):
+        if not label in balloons:
+          balloons.append(label)
+        break
+
+  #form an image of just those white areas with text candidate hits
+  mask = zeros(white_areas.shape,np.uint8)
+  for b in balloons:
+    #print "b : "+str(b)+" component " + str(components[balloons[b]])
+    print str(b)
+    two_d_slice = components[b]
+    mask[two_d_slice] |= labels[two_d_slice]==(b+1)
+    #also add nonzero pixels from all connected components ENTIRELY CONTAINED
+    #by this cc's bounding box. This is an attempt to partially recover smaller
+    #character components which might not be connected with the primary character
+    #(i.e. marks and accent like forms)
+    '''
+    if True:
+      for l in range(num_labels):
+        if l == label: continue
+        other_slice = components[l]
+        if contains(two_d_slice, other_slice):
+          mask[other_slice] |= labels[other_slice]==(l+1)
+    '''
+  return mask
 
 
 def main():
@@ -234,18 +189,31 @@ def main():
   binary[high_values] = 255
 
   area_mask = generate_mask(np.invert(binary), AreaFilter(min=10.0, max=100.0))
-  ar_mask = generate_mask(area_mask, AspectRatioFilter(min=0.8, max=1.2))
+  ar_mask = generate_mask(area_mask, AspectRatioFilter(min=0.75, max=1.25))
   clean_mask = np.invert(ar_mask)
   cleaned = np.invert(np.invert(image) * np.invert(clean_mask))
+
+  #okay. we've got a cleaned image that has some subset of CCs, each with a high probability
+  #of being a character or part of a character.
+  #If there are many of these in an individual large white area, the likelihood of that area
+  #being a word balloon or caption is very high.
+  #1 Generate a binary image off the final cleaned mask
+  cleaned_binary = np.invert(np.invert(binary) * np.invert(clean_mask))
+
+  #2 find all ares of white (or just large ones?)
+  white_areas = generate_mask(binary, AreaFilter(min=100.0, max=None), include_contained=False)
+
+  #3 isolate white areas which have a significant number of character candidates witin them
+  candidate_balloons = find_balloons(cleaned_binary, white_areas)
 
   plt.subplot(141)
   plt.imshow(image, cmap=cm.Greys_r)
   plt.subplot(142)
-  plt.imshow(binary, cmap=cm.Greys_r)
+  plt.imshow(cleaned_binary, cmap=cm.Greys_r)
   plt.subplot(143)
-  plt.imshow(np.invert(area_mask), cmap=cm.Greys_r)
+  plt.imshow(white_areas, cmap=cm.Greys_r)
   plt.subplot(144)
-  plt.imshow(cleaned, cmap=cm.Greys_r)
+  plt.imshow(candidate_balloons, cmap=cm.Greys_r)
 
   plt.show()
 
